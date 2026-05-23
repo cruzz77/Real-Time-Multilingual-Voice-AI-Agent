14080b142ed5120507d5b44ed2a232d1988e10da
Aditya Chopra <nytc120605@gmail.com>
2026-05-23 23:04:50 +0530

    enable websocket streaming on render

--- api/server.py
+++ api/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi import WebSocket
 
 
 app = FastAPI()
@@ -18,3 +19,27 @@ async def root():
     return FileResponse(
         "frontend/index.html"
     )
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(
+    websocket: WebSocket
+):
+
+    await websocket.accept()
+
+    try:
+
+        while True:
+
+            await websocket.receive_bytes()
+
+            await websocket.send_json({
+                "transcript": "Audio received successfully",
+                "response": "WebSocket working correctly on Render",
+                "audio_base64": ""
+            })
+
+    except Exception as e:
+
+        print(e)
